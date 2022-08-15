b4b58f4c43467e350b0aeb6e4cb179a827a9dfc6
zakariaffoh <nachradf10@gmail.com>
2022-08-16 00:55:18 +0100

    models

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,4 +1,7 @@
 use actix_web::{get, web, App, HttpServer, Responder};
+extern crate models;
+
+use models::authz::{Group, Role};
 
 #[get("/hello/{name}")]
 async fn greet(name: web::Path<String>) -> impl Responder {
@@ -7,6 +10,8 @@ async fn greet(name: web::Path<String>) -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let dat = "data".to_string();
+    println!("{}", dat);
     HttpServer::new(|| App::new().service(greet))
         .bind(("127.0.0.1", 8080))?
         .run()
