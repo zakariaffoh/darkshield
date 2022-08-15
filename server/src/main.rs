0a316e4c5e4f8658d746b1cefeb3a3465f3a4e16
zakariaffoh <nachradf10@gmail.com>
2022-08-16 00:59:56 +0100

    Feature/za/add projects (#8)
    * add core, crypto projects
    
    * add core, crypto projects

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,7 @@
 use actix_web::{get, web, App, HttpServer, Responder};
 extern crate models;
 
-use models::authz::{Group, Role};
+use models::authz;
 
 #[get("/hello/{name}")]
 async fn greet(name: web::Path<String>) -> impl Responder {
