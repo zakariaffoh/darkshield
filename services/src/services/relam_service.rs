85a63ca756f7a46836f52c5aa4a525a05efb978e
zakariaffoh <nachradf10@gmail.com>
2022-08-20 15:00:43 +0100

    services catalog (#16)

--- services/src/services/relam_service.rs
+++ services/src/services/relam_service.rs
@@ -1,11 +1,14 @@
 use models::entities::realm::RealmModel;
+use store::providers::rds::client::postgres_client::DataBaseManager;
 
 #[allow(dead_code)]
-pub struct RealmService {}
+pub struct RealmService<'d> {
+    database: &'d DataBaseManager,
+}
 
-impl<'d> RealmService {
-    pub fn new() -> Self {
-        Self {}
+impl<'d> RealmService<'d> {
+    pub fn new(database: &'d DataBaseManager) -> Self {
+        Self { database: database }
     }
     pub async fn create_realm(&self, realm: RealmModel) {}
 }
