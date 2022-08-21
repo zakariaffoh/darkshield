9b5f57dd3e175fb047bddb23290188d798ab9367
zakariaffoh <nachradf10@gmail.com>
2022-08-21 15:38:51 +0100

    add connection pool and refactor services (#21)

--- models/src/entities/realm.rs
+++ models/src/entities/realm.rs
@@ -1,6 +1,8 @@
+use serde::{Deserialize, Serialize};
+
 use crate::auditable::AuditableModel;
 
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
 #[allow(dead_code)]
 pub struct RealmModel {
     pub realm_id: String,
@@ -8,4 +10,20 @@ pub struct RealmModel {
     pub display_name: String,
     pub enabled: bool,
     pub metadata: AuditableModel,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct RealmCreateModel {
+    pub realm_id: String,
+    pub name: String,
+    pub display_name: String,
+    pub enabled: bool,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct RealmUpdateModel {
+    pub realm_id: String,
+    pub name: String,
+    pub display_name: String,
+    pub enabled: bool,
 }
