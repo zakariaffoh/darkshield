993c79f04703981a0f1f9680eb22abf7a3282d83
zakariaffoh <nachradf10@gmail.com>
2022-10-20 17:40:29 +0100

    encryption algorithms (#69)

--- src/crypto/src/providers/cek.rs
+++ src/crypto/src/providers/cek.rs
@@ -1,125 +1,134 @@
 use crate::jose::jwe::{
-    JweAlgorithmProvider, DirectAlgorithmProvider, 
-    AesKeyWrapAlgorithmProvider, ECDKKeyEncryptionJweAlgorithmProvider, CekManagementAlgorithmEnum, 
-    RsaKeyEncryptionJweAlgorithmProvider, Rsa15AlgorithmProvider
+    AesKeyWrapAlgorithmProvider, CekManagementAlgorithmEnum, DirectAlgorithmProvider,
+    ECDKKeyEncryptionJweAlgorithmProvider, JweAlgorithmProvider,
+    RsaKeyEncryptionJweAlgorithmProvider,
 };
 
 pub trait CekManagementProvider {
     fn algorithm_provider(&self) -> Box<dyn JweAlgorithmProvider>;
 }
 
-
 pub struct DirCekManagementProvider;
 
 #[allow(dead_code)]
-impl DirCekManagementProvider{
-    fn id(&self) -> &'static str{
+impl DirCekManagementProvider {
+    fn id(&self) -> &'static str {
         "DIR"
     }
 }
 
-impl CekManagementProvider for DirCekManagementProvider{
+impl CekManagementProvider for DirCekManagementProvider {
     fn algorithm_provider(&self) -> Box<dyn JweAlgorithmProvider> {
-        Box::new(DirectAlgorithmProvider{})
+        Box::new(DirectAlgorithmProvider {})
     }
 }
 
 pub struct A128KWCekManagementProvider;
 
 #[allow(dead_code)]
-impl A128KWCekManagementProvider{
-    fn id(&self) -> &'static str{
+impl A128KWCekManagementProvider {
+    fn id(&self) -> &'static str {
         "A128KW"
     }
 }
 
-impl CekManagementProvider for A128KWCekManagementProvider{
+impl CekManagementProvider for A128KWCekManagementProvider {
     fn algorithm_provider(&self) -> Box<dyn JweAlgorithmProvider> {
-        Box::new(AesKeyWrapAlgorithmProvider{})
+        Box::new(AesKeyWrapAlgorithmProvider {})
     }
 }
 
 pub struct Rsa15CekManagementProvider;
 
 #[allow(dead_code)]
-impl Rsa15CekManagementProvider{
-    fn id(&self) -> &'static str{
+impl Rsa15CekManagementProvider {
+    fn id(&self) -> &'static str {
         "RSA1_5"
     }
 }
 
-impl CekManagementProvider for Rsa15CekManagementProvider{
+impl CekManagementProvider for Rsa15CekManagementProvider {
     fn algorithm_provider(&self) -> Box<dyn JweAlgorithmProvider> {
-        Box::new(Rsa15AlgorithmProvider{})
+        Box::new(RsaKeyEncryptionJweAlgorithmProvider::new(
+            &CekManagementAlgorithmEnum::Rsa1_5.to_string(),
+        ))
     }
 }
 
 pub struct RSAOAEPCekManagementProvider;
 
 #[allow(dead_code)]
-impl RSAOAEPCekManagementProvider{
-    fn id(&self) -> &'static str{
+impl RSAOAEPCekManagementProvider {
+    fn id(&self) -> &'static str {
         "RSA-OAEP"
     }
 }
 
-impl CekManagementProvider for RSAOAEPCekManagementProvider{
+impl CekManagementProvider for RSAOAEPCekManagementProvider {
     fn algorithm_provider(&self) -> Box<dyn JweAlgorithmProvider> {
-        Box::new(RsaKeyEncryptionJweAlgorithmProvider::new(CekManagementAlgorithmEnum::RsaOaep))
+        Box::new(RsaKeyEncryptionJweAlgorithmProvider::new(
+            &CekManagementAlgorithmEnum::RsaOaep.to_string(),
+        ))
     }
 }
 
 pub struct RSAOAEP256CekManagementProvider;
 
 #[allow(dead_code)]
-impl RSAOAEP256CekManagementProvider{
-    fn id(&self) -> &'static str{
+impl RSAOAEP256CekManagementProvider {
+    fn id(&self) -> &'static str {
         "RSA-OAEP-256"
     }
 }
 
-impl CekManagementProvider for RSAOAEP256CekManagementProvider{
+impl CekManagementProvider for RSAOAEP256CekManagementProvider {
     fn algorithm_provider(&self) -> Box<dyn JweAlgorithmProvider> {
-        Box::new(RsaKeyEncryptionJweAlgorithmProvider::new(CekManagementAlgorithmEnum::RsaOaep256))
+        Box::new(RsaKeyEncryptionJweAlgorithmProvider::new(
+            &CekManagementAlgorithmEnum::RsaOaep256.to_string(),
+        ))
     }
 }
 
 pub struct ECDHESCekManagementProvider;
-    
+
 #[allow(dead_code)]
-impl ECDHESCekManagementProvider{
-    fn id(&self) -> &'static str{
+impl ECDHESCekManagementProvider {
+    fn id(&self) -> &'static str {
         "ECDH-ES"
     }
 }
 
-impl CekManagementProvider for ECDHESCekManagementProvider{
+impl CekManagementProvider for ECDHESCekManagementProvider {
     fn algorithm_provider(&self) -> Box<dyn JweAlgorithmProvider> {
-        Box::new(ECDKKeyEncryptionJweAlgorithmProvider::new(CekManagementAlgorithmEnum::EcdhEs))
+        Box::new(ECDKKeyEncryptionJweAlgorithmProvider::new(
+            &CekManagementAlgorithmEnum::EcdhEs.to_string(),
+        ))
     }
 }
 
 pub struct ECDHESA128KWCekManagementProvider;
 
 #[allow(dead_code)]
-impl ECDHESA128KWCekManagementProvider{
-    fn id(&self) -> &'static str{
+impl ECDHESA128KWCekManagementProvider {
+    fn id(&self) -> &'static str {
         "ECDH-ES+A128KW"
     }
 }
 
-impl CekManagementProvider for ECDHESA128KWCekManagementProvider{
+impl CekManagementProvider for ECDHESA128KWCekManagementProvider {
     fn algorithm_provider(&self) -> Box<dyn JweAlgorithmProvider> {
-        Box::new(ECDKKeyEncryptionJweAlgorithmProvider::new(CekManagementAlgorithmEnum::EcdhEsA128kw))
+        Box::new(ECDKKeyEncryptionJweAlgorithmProvider::new(
+            &CekManagementAlgorithmEnum::EcdhEsA128kw.to_string(),
+        ))
     }
 }
 
 pub struct CekManagementProviderFactory;
 
-impl CekManagementProviderFactory{
+impl CekManagementProviderFactory {
     pub fn encryption_provider(
-        algorithm: &CekManagementAlgorithmEnum
-    ) -> Box<dyn CekManagementProvider>{
+        algorithm: &CekManagementAlgorithmEnum,
+    ) -> Box<dyn CekManagementProvider> {
         match &algorithm {
             CekManagementAlgorithmEnum::Dir => Box::new(DirCekManagementProvider),
             CekManagementAlgorithmEnum::Rsa1_5 => Box::new(Rsa15CekManagementProvider),
@@ -131,7 +140,7 @@ impl CekManagementProviderFactory{
         }
     }
 
-    pub fn supported_algorithms() -> Vec<String>{
+    pub fn supported_algorithms() -> Vec<String> {
         vec![
             CekManagementAlgorithmEnum::Dir.to_string(),
             CekManagementAlgorithmEnum::Rsa1_5.to_string(),
