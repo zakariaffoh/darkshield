033f3142ef07f9f42b35e1420d7416a2c55d5fed
zakariaffoh <nachradf10@gmail.com>
2022-10-23 20:56:25 +0100

    pbkdf2 refactoring

--- src/crypto/src/pbkdf/mod.rs
+++ src/crypto/src/pbkdf/mod.rs
@@ -1,9 +1,14 @@
-use data_encoding::HEXUPPER;
-use ring::pbkdf2;
-use std::num::NonZeroU32;
-use std::str;
+pub mod pbkdf2_error;
 
-#[derive(PartialEq, Eq)]
+use anyhow::{self, bail};
+use openssl::{memcmp::eq, pkcs5};
+use std::fmt::Debug;
+use std::fmt::Display;
+
+use self::pbkdf2_error::PBKDF2Error;
+use crate::jose::util::hash_algorithm::HashAlgorithm;
+
+#[derive(Debug, Eq, PartialEq, Copy, Clone)]
 pub enum PBKDF2AlgorithmEnum {
     Pbkdf2HmacSha1,
     Pbkdf2HmacSha256,
@@ -11,142 +16,171 @@ pub enum PBKDF2AlgorithmEnum {
     Pbkdf2HmacSha512,
 }
 
-impl ToString for PBKDF2AlgorithmEnum {
-    fn to_string(&self) -> String {
+impl PBKDF2AlgorithmEnum {
+    pub fn name(&self) -> &str {
+        match &self {
+            Self::Pbkdf2HmacSha1 => "pbkdf2-sha1",
+            Self::Pbkdf2HmacSha256 => "pbkdf2-sha256",
+            Self::Pbkdf2HmacSha384 => "pbkdf2-sha384",
+            Self::Pbkdf2HmacSha512 => "pbkdf2-sha512",
+        }
+    }
+
+    pub fn pbkdf2_hasher(&self, algorithm: &str) -> Result<Box<dyn PBKDF2Hasher>, PBKDF2Error> {
+        (|| -> anyhow::Result<Box<dyn PBKDF2Hasher>> {
+            if algorithm.to_lowercase().as_str() != self.name() {
+                bail!("{}", algorithm.to_lowercase())
+            }
+            match algorithm.to_lowercase().as_str() {
+                "pbkdf2-sha1" => Ok(Box::new(PBKDF2HasherImp::new(Self::Pbkdf2HmacSha1))),
+                "pbkdf2-sha256" => Ok(Box::new(PBKDF2HasherImp::new(Self::Pbkdf2HmacSha1))),
+                "pbkdf2-sha384" => Ok(Box::new(PBKDF2HasherImp::new(Self::Pbkdf2HmacSha1))),
+                "pbkdf2-sha512" => Ok(Box::new(PBKDF2HasherImp::new(Self::Pbkdf2HmacSha1))),
+                _ => bail!("{}", algorithm.to_lowercase()),
+            }
+        })()
+        .map_err(|err| match err.downcast::<PBKDF2Error>() {
+            Ok(err) => err,
+            Err(err) => PBKDF2Error::UnsupportedPBKDF2HashAlgorithm(err),
+        })
+    }
+
+    pub fn pbkdf2_verifier(&self, algorithm: &str) -> Result<Box<dyn PBKDF2Verifier>, PBKDF2Error> {
+        (|| -> anyhow::Result<Box<dyn PBKDF2Verifier>> {
+            if algorithm.to_lowercase().as_str() != self.name() {
+                bail!("{}", algorithm.to_lowercase())
+            }
+            match algorithm.to_lowercase().as_str() {
+                "pbkdf2-sha1" => Ok(Box::new(PBKDF2VerifierImp::new(Self::Pbkdf2HmacSha1))),
+                "pbkdf2-sha256" => Ok(Box::new(PBKDF2VerifierImp::new(Self::Pbkdf2HmacSha1))),
+                "pbkdf2-sha384" => Ok(Box::new(PBKDF2VerifierImp::new(Self::Pbkdf2HmacSha1))),
+                "pbkdf2-sha512" => Ok(Box::new(PBKDF2VerifierImp::new(Self::Pbkdf2HmacSha1))),
+                _ => bail!("{}", algorithm.to_lowercase()),
+            }
+        })()
+        .map_err(|err| match err.downcast::<PBKDF2Error>() {
+            Ok(err) => err,
+            Err(err) => PBKDF2Error::UnsupportedPBKDF2VerifierAlgorithm(err),
+        })
+    }
+
+    pub fn hash_algorithm(&self) -> HashAlgorithm {
         match self {
-            PBKDF2AlgorithmEnum::Pbkdf2HmacSha1 => "pbkdf24".to_owned(),
-            PBKDF2AlgorithmEnum::Pbkdf2HmacSha256 => "pbkdf2-sha256".to_owned(),
-            PBKDF2AlgorithmEnum::Pbkdf2HmacSha384 => "pbkdf2-sha384".to_owned(),
-            PBKDF2AlgorithmEnum::Pbkdf2HmacSha512 => "pbkdf2-sha512".to_owned(),
+            Self::Pbkdf2HmacSha1 => HashAlgorithm::Sha1,
+            Self::Pbkdf2HmacSha256 => HashAlgorithm::Sha256,
+            Self::Pbkdf2HmacSha384 => HashAlgorithm::Sha384,
+            Self::Pbkdf2HmacSha512 => HashAlgorithm::Sha512,
+        }
+    }
+
+    pub fn derived_key_len(&self) -> usize {
+        match self {
+            Self::Pbkdf2HmacSha1 => 64,
+            Self::Pbkdf2HmacSha256 => 64,
+            Self::Pbkdf2HmacSha384 => 64,
+            Self::Pbkdf2HmacSha512 => 64,
         }
     }
 }
 
-pub struct PBKDF2HashProvider(PBKDF2AlgorithmEnum);
+impl Display for PBKDF2AlgorithmEnum {
+    fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+        fmt.write_str(self.name())
+    }
+}
 
-impl PBKDF2HashProvider {
+pub trait PBKDF2Hasher: Debug + Send + Sync {
+    fn algorithm(&self) -> &PBKDF2AlgorithmEnum;
+
+    fn derive(&self, password: &[u8], salt: &[u8], iterations: u32)
+        -> Result<Vec<u8>, PBKDF2Error>;
+}
+
+#[derive(Debug)]
+pub struct PBKDF2HasherImp {
+    algorithm: PBKDF2AlgorithmEnum,
+}
+
+impl PBKDF2HasherImp {
     pub fn new(algorithm: PBKDF2AlgorithmEnum) -> Self {
-        Self(algorithm)
-    }
-
-    pub fn derive(
-        &self,
-        password: &str,
-        salt: &str,
-        iterations: u32,
-        derived_key_size: u32,
-    ) -> String {
-        let algorithm = self.get_algorithm();
-        let encoded_salt = HEXUPPER.decode(&salt.as_bytes()).unwrap();
-        let iters = NonZeroU32::new(iterations).unwrap();
-        let mut hashed_cred_store = Vec::<u8>::new();
-        hashed_cred_store.resize(derived_key_size as usize, 0);
-        pbkdf2::derive(
-            algorithm,
-            iters,
-            &encoded_salt,
-            password.as_bytes(),
-            &mut hashed_cred_store,
-        );
-
-        return HEXUPPER.encode(&hashed_cred_store);
-    }
-
-    pub fn verify(
-        self,
-        encoded_password: &str,
-        raw_password: &str,
-        salt: &str,
-        iterations: u32,
-    ) -> bool {
-        let algorithm = self.get_algorithm();
-        let encoded_salt = HEXUPPER.decode(&salt.as_bytes()).unwrap();
-        let iters = NonZeroU32::new(iterations).unwrap();
-
-        let response = pbkdf2::verify(
-            algorithm,
-            iters,
-            &encoded_salt,
-            &raw_password.as_bytes(),
-            &HEXUPPER.decode(&encoded_password.as_bytes()).unwrap(),
-        );
-        if let Ok(_) = response {
-            return true;
-        }
-        return false;
-    }
-
-    fn get_algorithm(&self) -> pbkdf2::Algorithm {
-        let algo = &self.0;
-        if algo == &PBKDF2AlgorithmEnum::Pbkdf2HmacSha1 {
-            pbkdf2::PBKDF2_HMAC_SHA1.clone()
-        } else if algo == &PBKDF2AlgorithmEnum::Pbkdf2HmacSha256 {
-            pbkdf2::PBKDF2_HMAC_SHA256.clone()
-        } else if algo == &PBKDF2AlgorithmEnum::Pbkdf2HmacSha384 {
-            pbkdf2::PBKDF2_HMAC_SHA384.clone()
-        } else {
-            pbkdf2::PBKDF2_HMAC_SHA512.clone()
+        Self {
+            algorithm: algorithm,
         }
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use ring::rand::{self, SecureRandom};
-
-    fn salt_generator() -> String {
-        let mut salt = [0u8; 64];
-        let rng = rand::SystemRandom::new();
-        rng.fill(&mut salt).unwrap();
-        HEXUPPER.encode(&salt)
+impl PBKDF2Hasher for PBKDF2HasherImp {
+    fn algorithm(&self) -> &PBKDF2AlgorithmEnum {
+        &self.algorithm
     }
 
-    #[test]
-    fn test_pdbdk() {
-        let password = "password";
-        let iterations = 100_000;
-        let salt = "F18E70D777B93C9B6B66D4CA563D9563EC08CAAD48003C06DAB1C37BA92D7EF03EC85B168B8F30481DF9D2EBE01A6935DA329A8510AE2954FEC2A23769A847B9";
-        {
-            let hasher = PBKDF2HashProvider::new(PBKDF2AlgorithmEnum::Pbkdf2HmacSha1);
-            let response = hasher.derive(password, salt, iterations, 32);
-            assert_eq!(
-                response,
-                "63ABF9763FD968DD8A7A07230D2CFF5A1CDAA7C0EAB97134A0E02A2FC45AF7C2"
-            );
-            assert_eq!(
-                hasher.verify(
-                    "63ABF9763FD968DD8A7A07230D2CFF5A1CDAA7C0EAB97134A0E02A2FC45AF7C2",
-                    password,
-                    salt,
-                    iterations
-                ),
-                true
-            );
+    fn derive(
+        &self,
+        password: &[u8],
+        salt: &[u8],
+        iterations: u32,
+    ) -> Result<Vec<u8>, PBKDF2Error> {
+        (|| -> anyhow::Result<Vec<u8>> {
+            let md = self.algorithm.hash_algorithm().message_digest();
+            let mut derived_key = vec![0; self.algorithm.derived_key_len()];
+            let p2c = iterations as usize;
+            pkcs5::pbkdf2_hmac(&password, &salt, p2c, md, &mut derived_key)?;
+            Ok(derived_key)
+        })()
+        .map_err(|err| match err.downcast::<PBKDF2Error>() {
+            Ok(err) => err,
+            Err(err) => PBKDF2Error::UnsupportedPBKDF2HashAlgorithm(err),
+        })
+    }
+}
+
+pub trait PBKDF2Verifier: Debug + Send + Sync {
+    fn algorithm(&self) -> &PBKDF2AlgorithmEnum;
+
+    fn verify(
+        &self,
+        encoded_password: &[u8],
+        password: &[u8],
+        salt: &[u8],
+        iterations: u32,
+    ) -> Result<bool, PBKDF2Error>;
+}
+
+#[derive(Debug)]
+pub struct PBKDF2VerifierImp {
+    algorithm: PBKDF2AlgorithmEnum,
+}
+
+impl PBKDF2VerifierImp {
+    pub fn new(algorithm: PBKDF2AlgorithmEnum) -> Self {
+        Self {
+            algorithm: algorithm,
         }
-        {
-            let hasher = PBKDF2HashProvider::new(PBKDF2AlgorithmEnum::Pbkdf2HmacSha256);
-            let response = hasher.derive(password, salt, iterations, 32);
-            assert_eq!(
-                response,
-                "315C5AEF98B9E9117FB2022BABD6A28728F32BECDD56E3ED9F03FBEAE644A82F"
-            );
-        }
-        {
-            let hasher = PBKDF2HashProvider::new(PBKDF2AlgorithmEnum::Pbkdf2HmacSha384);
-            let response = hasher.derive(password, salt, iterations, 32);
-            assert_eq!(
-                response,
-                "01CEBC9B2B6B20A081BB000759E180D13B2B3886E652D82947170D7E821DDD20"
-            );
-        }
-        {
-            let hasher = PBKDF2HashProvider::new(PBKDF2AlgorithmEnum::Pbkdf2HmacSha512);
-            let response = hasher.derive(password, salt, iterations, 32);
-            assert_eq!(
-                response,
-                "905814FB4F6C953A86FDCEDCDB2C2AB9D0B57BA52CCC78F4C8847B1A300A6403"
-            );
-        }
+    }
+}
+
+impl PBKDF2Verifier for PBKDF2VerifierImp {
+    fn algorithm(&self) -> &PBKDF2AlgorithmEnum {
+        &self.algorithm
+    }
+
+    fn verify(
+        &self,
+        encoded_password: &[u8],
+        password: &[u8],
+        salt: &[u8],
+        iterations: u32,
+    ) -> Result<bool, PBKDF2Error> {
+        (|| -> anyhow::Result<bool> {
+            let md = self.algorithm.hash_algorithm().message_digest();
+            let mut derived_key = vec![0; self.algorithm.derived_key_len()];
+            let p2c = iterations as usize;
+            pkcs5::pbkdf2_hmac(&password, &salt, p2c, md, &mut derived_key)?;
+            Ok(eq(&derived_key, &encoded_password))
+        })()
+        .map_err(|err| match err.downcast::<PBKDF2Error>() {
+            Ok(err) => err,
+            Err(err) => PBKDF2Error::UnsupportedPBKDF2VerifierAlgorithm(err),
+        })
     }
 }
